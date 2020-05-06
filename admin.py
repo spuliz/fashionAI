ce1e15f1e3229da39c3ab17961c6272b5ecfd40b
Saverio Pulizzi <saverio.pulizzi91@gmail.com>
2020-05-06 17:14:49 +0100

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Uploadpics, Styles, UploadStylePics
+# admin.site.register(Document)
+admin.site.register(Uploadpics)
+admin.site.register(Styles)
+admin.site.register(UploadStylePics)
+# Register your models here.
